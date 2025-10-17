3bd020c3eba94012756eee5ce609f987683f1aa1
George0712 <jorge12072002@gmail.com>
2025-10-17 11:12:02 -0500

    feat: implementar campos funcionales y ubicar componente footer

--- src/app/features/registrar-acuerdos/components/datos-contacto/datos-contacto.component.ts
+++ src/app/features/registrar-acuerdos/components/datos-contacto/datos-contacto.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-datos-contacto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './datos-contacto.component.html',
   styleUrl: './datos-contacto.component.scss'
 })
 export class DatosContactoComponent {
+  // Apoderado
+  registrarApoderado = false;
+  nombreApoderado = '';
+  tipoDocumentoApoderado = '';
+  numeroDocumentoApoderado = '';
+  telefonoApoderado = '';
+  correoApoderado = '';
+  tarjetaProfesionalApoderado = '';
 
+  // Datos de ubicación
+  usarMismaDireccion = false;
+  direccion = '';
+  departamento = '';
+  ciudad = '';
+  telefonoAlternativo = '';
+  correoAdicional = '';
 }
--- src/app/layouts/main/main-layout/main-layout.component.ts
+++ src/app/layouts/main/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../../shared/components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
